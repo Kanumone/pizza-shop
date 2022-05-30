f7bec10ac23873f2df8ccaea0ab18177697d8f97
Kanumone <KuzminAAwork@yandex.ru>
2022-05-30 11:22:38 +0700

    Create Skeleton for loading pizzas

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
+import Skeleton from './components/PizzaBlock/Skeleton';
 
 function App() {
   let [items, setItems] = React.useState([]);
+  let [isLoad, setLoadStatus] = React.useState(false);
   React.useEffect(() => {
     fetch('https://62939adb089f87a57ac32c89.mockapi.io/Pizzas')
       .then((res) => res.json())
-      .then((data) => setItems(data));
-  }, []);
+      .then((data) => {
+        setItems(data);
+        setLoadStatus(true);
+      }, []);
+  });
 
   return (
     <div className="App">
@@ -25,9 +30,9 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {items.map((item) => (
-                <PizzaBlock key={item.imageUrl} {...item} />
-              ))}
+              {isLoad
+                ? items.map((item) => <PizzaBlock key={item.imageUrl} {...item} />)
+                : [...new Array(6)].map((_, index) => <Skeleton key={index} />)}
             </div>
           </div>
         </div>
---
+++ src/components/PizzaBlock/Skeleton.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ContentLoader from 'react-content-loader';
+
+const Skeleton = () => (
+  <ContentLoader
+    className="pizza-block"
+    speed={2}
+    width={280}
+    height={470}
+    viewBox="0 0 280 470"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb">
+    <circle cx="135" cy="130" r="128" />
+    <rect x="0" y="268" rx="10" ry="10" width="280" height="27" />
+    <rect x="0" y="315" rx="10" ry="10" width="280" height="88" />
+    <rect x="0" y="430" rx="10" ry="10" width="90" height="27" />
+    <rect x="123" y="420" rx="25" ry="25" width="152" height="47" />
+  </ContentLoader>
+);
+
+export default Skeleton;
