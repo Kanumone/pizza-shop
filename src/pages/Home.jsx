7700844fc0666ab7ad3be033abf0cfc4c96333d4
Kanumone <KuzminAAwork@yandex.ru>
2022-06-02 18:23:12 +0700

    Add search by name logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,17 +5,27 @@ import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import Skeleton from '../components/PizzaBlock/Skeleton';
 
+import { SearchContext } from '../App';
+
 function Home() {
   let [items, setItems] = React.useState([]);
   let [isLoad, setLoadStatus] = React.useState(false);
+
+  const { searchValue } = React.useContext(SearchContext);
+
+  const pizzas = items.map((item) => <PizzaBlock key={item.imageUrl} {...item} />);
+  const skeleton = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+
   React.useEffect(() => {
-    fetch('https://62939adb089f87a57ac32c89.mockapi.io/Pizzas')
+    const search = searchValue ? `?search=${searchValue}` : '';
+
+    fetch(`https://62939adb089f87a57ac32c89.mockapi.io/Pizzas${search}`)
       .then((res) => res.json())
       .then((data) => {
         setItems(data);
         setLoadStatus(true);
       });
-  }, []);
+  }, [searchValue]);
 
   return (
     <>
@@ -24,11 +34,7 @@ function Home() {
         <Sort />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoad
-          ? items.map((item) => <PizzaBlock key={item.imageUrl} {...item} />)
-          : [...new Array(6)].map((_, index) => <Skeleton key={index} />)}
-      </div>
+      <div className="content__items">{isLoad ? pizzas : skeleton}</div>
     </>
   );
 }
